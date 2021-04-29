dfad1146faeba78cf5310b3cb100f7b55f71ff66
Umbardacil <kevin.myung.gaetan.jordan@eisti.eu>
2021-04-29 16:03:42 +0200

    Classes and views

--- __manifest__.py
+++ __manifest__.py
@@ -1,14 +1,10 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "test-module",
+    'name': "Determinant test",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
-    """,
+        Short test for recruitment purpose for Determinant.
+		""",
 
     'author': "My Company",
     'website': "http://www.yourcompany.com",
@@ -16,7 +12,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Test',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
--- models/models.py
+++ models/models.py
@@ -1,7 +1,29 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import fields, models
+# from odoo import api
 
+
+class Alpha(models.Model):
+	_name = "test-module.alpha"
+	
+	name = fields.Char(required=true)
+	date = fields.Date(string="Starting date")
+	address = fields.Char()
+	key = fields.Char()
+	betas = fields.One2Many(
+		'test-module.beta',
+		'alpha_id',
+		ondelete='set null'
+	)
+	
+	
+class Beta(models.Model):
+	_name = "test-module.beta"
+	
+	name = fields.Char(string="Name", required=true)
+	phone = fields.Char(string="Phone number")
+	price = fields.Integer(string="Price")
 
 # class test-module(models.Model):
 #     _name = 'test-module.test-module'
