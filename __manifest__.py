8a71e6b3b1f0f06aec2796aee520b329e7711f49
jordankevi <jordan.kevin57@gmail.com>
2021-04-29 18:28:00 +0200

    Quick manifest update

--- __manifest__.py
+++ __manifest__.py
@@ -6,8 +6,8 @@
         Short test for recruitment purpose for Determinant.
 		""",
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "JORDAN Kévin",
+    'website': "http://racontard.fr",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -24,8 +24,5 @@
         'views/views.xml',
         'views/templates.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+
 }
