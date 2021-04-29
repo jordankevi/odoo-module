30bc2f012bb1cbd23195c5eb13fab6db65c6f5b5
Umbardacil <kevin.myung.gaetan.jordan@eisti.eu>
2021-04-29 16:23:44 +0200

    Setup controller and update README

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # odoo-module
 Technical test in odoo environment
+
+## Disclaimer
+
+Unable to install Odoo properly on Linux nor Windows, I made this module without being able to test it, seeing if it works or not (pretty sure it won't). Sorry for the inconvenience.
--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,21 +1,38 @@
 # -*- coding: utf-8 -*-
-# from odoo import http
+from odoo import http
 
 
-# class Test-module(http.Controller):
-#     @http.route('/test-module/test-module/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class Test-module(http.Controller):
+    @http.route('/test-module/', auth='public')
+    def index(self, **kwargs):
+        return "Bienvenue sur le module de test"
 
-#     @http.route('/test-module/test-module/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('test-module.listing', {
-#             'root': '/test-module/test-module',
-#             'objects': http.request.env['test-module.test-module'].search([]),
-#         })
+class Alpha-module(http.Controller):
+	@http.route('test-module/alpha/objects', auth="public")
+	def list(self, **kwargs):
+		return http.request.render(
+			'test-module.listing',
+			{
+				'root': '/test-module/alpha',
+				'objects': http.request.env['test-module.alpha].search([]),
+			}
+		)
+	
+	@http.route('test-module/alpha/objects/<model("test-module.alpha"):obj>', auth='public')
+	def object(self, obj, **kargs):
+		return http.request.render('test-module.object', {'object', obj})
 
-#     @http.route('/test-module/test-module/objects/<model("test-module.test-module"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('test-module.object', {
-#             'object': obj
-#         })
+class beta-module(http.Controller):
+	@http.route('test-module/beta/objects', auth="public")
+	def list(self, **kwargs):
+		return http.request.render(
+			'test-module.listing',
+			{
+				'root': '/test-module/beta',
+				'objects': http.request.env['test-module.beta].search([]),
+			}
+		)
+	
+	@http.route('test-module/beta/objects/<model("test-module.beta"):obj>', auth='public')
+	def object(self, obj, **kargs):
+		return http.request.render('test-module.object', {'object', obj})
